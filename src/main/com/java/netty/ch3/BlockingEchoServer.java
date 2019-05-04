3512243c813c8e58b5fe8925d3776637fa185f42
keunwon <keunwon.c2@gmail.com>
2019-05-04 18:25:14 +0900

    Netty BootStrap study

---
+++ src/main/com/java/netty/ch3/BlockingEchoServer.java
@@ -0,0 +1,38 @@
+package com.java.netty.ch3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+
+public class BlockingEchoServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new OioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new OioEventLoopGroup();
+
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workerGroup)
+                    .channel(OioServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = server.bind(8888).sync();
+            f.channel().closeFuture().sync();
+        }
+        finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/com/java/netty/ch3/EpollEchoServer.java
@@ -0,0 +1,38 @@
+package com.java.netty.ch3;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.epoll.EpollEventLoopGroup;
+import io.netty.channel.epoll.EpollServerSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+
+public class EpollEchoServer {
+
+    public static void main(String[] args) throws Exception {
+        EventLoopGroup bossGroup = new EpollEventLoopGroup(1);
+        EventLoopGroup workerGroup = new EpollEventLoopGroup();
+
+        try {
+            ServerBootstrap server = new ServerBootstrap();
+            server.group(bossGroup, workerGroup)
+                    .channel(EpollServerSocketChannel.class)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new EchoServerHandler());
+                        }
+                    });
+
+            ChannelFuture f = server.bind(8888).sync();
+            f.channel().closeFuture().sync();
+        }
+        finally {
+            workerGroup.shutdownGracefully();
+            bossGroup.shutdownGracefully();
+        }
+    }
+}
