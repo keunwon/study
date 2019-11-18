e4541fd88927c05b0bb9de06072c0348abd316bf
keunwon <keunwon.c2@gmail.com>
2019-11-18 23:37:22 +0900

    05. 책임 할당하기

--- src/com/keunwon/object/chapter05/step01/Customer.java
+++ src/com/keunwon/object/chapter05/step01/Customer.java
@@ -1,4 +1,11 @@
 package com.keunwon.object.chapter05.step01;
 
 public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(String name, String id) {
+        this.id = id;
+        this.name = name;
+    }
 }
