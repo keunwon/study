563360c6bdb7b99089954de72a109c7e399f003f
keunwon <keunwon.c2@gmail.com>
2019-11-24 21:38:53 +0900

    09. 유연한 설계

---
+++ src/com/keunwon/object/chapter09/step01/pricing/PeriodCondition.java
@@ -0,0 +1,26 @@
+package com.keunwon.object.chapter09.step01.pricing;
+
+import com.keunwon.object.chapter09.step01.DiscountCondition;
+import com.keunwon.object.chapter09.step01.Screening;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class PeriodCondition implements DiscountCondition {
+    private DayOfWeek dayOfWeek;
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public PeriodCondition(DayOfWeek dayOfWeek, LocalTime startTime, LocalTime endTime) {
+        this.dayOfWeek = dayOfWeek;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.getStartTime().getDayOfWeek().equals(dayOfWeek) &&
+                startTime.compareTo(screening.getStartTime().toLocalTime()) <= 0&&
+                endTime.compareTo(screening.getStartTime().toLocalTime()) >= 0;
+    }
+}
