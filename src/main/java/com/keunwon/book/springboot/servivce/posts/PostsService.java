49e23d390a42ddb24bdc0cc1d6f0618008279fb5
keunwon <keunwon.c2@gmail.com>
2019-12-31 00:24:35 +0900

    게시판 화면 및 기능 추가

--- src/main/java/com/keunwon/book/springboot/servivce/posts/PostsService.java
+++ src/main/java/com/keunwon/book/springboot/servivce/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.keunwon.book.springboot.servivce.posts;
 
 import com.keunwon.book.springboot.domain.posts.Posts;
 import com.keunwon.book.springboot.domain.posts.PostsRepository;
+import com.keunwon.book.springboot.web.dto.PostsListResponseDto;
 import com.keunwon.book.springboot.web.dto.PostsResponseDto;
 import com.keunwon.book.springboot.web.dto.PostsSaveRequestDto;
 import com.keunwon.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +37,20 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(posts);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                    .map(PostsListResponseDto::new)
+                    .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 게스글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
     }
 }
