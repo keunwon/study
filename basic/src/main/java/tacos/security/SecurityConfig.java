84a0249c9e54a4762e0280c1521604af3ad9bac3
keunwon <keunwon.c2@gmail.com>
2020-06-27 23:46:34 +0900

    04. 스프링 시큐리티

---
+++ basic/src/main/java/tacos/security/SecurityConfig.java
@@ -0,0 +1,56 @@
+package tacos.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserRepositoryUserDetailService userService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/design", "/orders").hasRole("USER")
+                    .antMatchers("/", "/**").permitAll()
+                .and()
+                    .formLogin()
+                        .loginPage("/login")
+                        .loginProcessingUrl("/authenticate")
+                        .usernameParameter("user")
+                        .passwordParameter("pwd")
+                        .defaultSuccessUrl("/design", true)
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userService)
+                .passwordEncoder(new BCryptPasswordEncoder());
+    }
+
+    // 인메모리 사용자 스토어
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                    .withUser("user1")
+                    .password("{noop}password1")
+                    .authorities("ROLE_USER")
+                .and()
+                    .withUser("user2")
+                    .password("{noop}password2")
+                    .authorities("ROLE_USER");
+    }*/
+
+
+}
