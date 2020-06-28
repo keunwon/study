c45d7d33d300a875dfc2ed55094392d5ec085ad5
keunwon <keunwon.c2@gmail.com>
2020-06-29 00:36:16 +0900

    05. 구성 속성 사용하기

--- basic/src/main/java/tacos/web/OrderController.java
+++ basic/src/main/java/tacos/web/OrderController.java
@@ -2,8 +2,11 @@ package tacos.web;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
@@ -19,6 +22,7 @@ import javax.validation.Valid;
 @SessionAttributes("order")
 public class OrderController {
     private final OrderRepository orderRepository;
+    private final OrderProps orderProps;
 
     @GetMapping("/current")
     public String orderForm(@AuthenticationPrincipal User user, @ModelAttribute Order order) {
@@ -55,5 +59,13 @@ public class OrderController {
         sessionStatus.setComplete();
         log.info("Order submitted: " + order);
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String ordersForUser(@AuthenticationPrincipal User user, Model model) {
+        Pageable pageable = PageRequest.of(0, orderProps.getPageSize());
+        model.addAttribute("orders", orderRepository.findByUserOrderByPlacedAtDesc(user, pageable));
+
+        return "orderList";
     }
 }
