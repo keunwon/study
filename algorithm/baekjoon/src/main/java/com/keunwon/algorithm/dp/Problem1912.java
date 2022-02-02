f205d7e3d11a04e576068b1f2bea59d01eddbefc
keunwon <keunwon.c2@gmail.com>
2022-02-03 02:27:37 +0900

    백준 1912 풀이

---
+++ algorithm/baekjoon/src/main/java/com/keunwon/algorithm/dp/Problem1912.java
@@ -0,0 +1,34 @@
+package com.keunwon.algorithm.dp;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Problem1912 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        final int n = Integer.parseInt(br.readLine());
+        final int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        final int[] dp = new int[n];
+        dp[0] = arr[0];
+
+        int max = arr[0];
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1] + arr[i], arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+
+        bw.write(max + System.lineSeparator());
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
