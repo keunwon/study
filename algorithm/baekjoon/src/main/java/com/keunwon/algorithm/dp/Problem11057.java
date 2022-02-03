d0223278fb20bc74ee7771e8f005af8ba3ff794b
keunwon <keunwon.c2@gmail.com>
2022-02-03 22:19:04 +0900

    백준 1149 풀이

--- algorithm/baekjoon/src/main/java/com/keunwon/algorithm/dp/Problem11057.java
+++ algorithm/baekjoon/src/main/java/com/keunwon/algorithm/dp/Problem11057.java
@@ -1,6 +1,6 @@
 package com.keunwon.algorithm.dp;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 public class Problem11057 {
@@ -58,5 +58,33 @@ public class Problem11057 {
         }
 
         System.out.println(result % 10_007);
+    }
+
+    private static void other2() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        final int divide = 10_007;
+        final int n = Integer.parseInt(br.readLine());
+        final int[][] dp = new int[n + 1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = 1;
+
+            for (int j = 1; j <= 9; j++) {
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+            }
+        }
+
+        long result = 0;
+        for (int i = 0; i <= 9; i++) {
+            result += dp[n][i];
+        }
+
+        System.out.println(result % divide);
     }
 }
