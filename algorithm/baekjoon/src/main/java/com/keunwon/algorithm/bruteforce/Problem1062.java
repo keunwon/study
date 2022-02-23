f425b836b23949055d18357fec86d147aeb42ab9
keunwon <keunwon.c2@gmail.com>
2022-02-23 22:22:42 +0900

    bruteforce 문제 풀이

---
+++ algorithm/baekjoon/src/main/java/com/keunwon/algorithm/bruteforce/Problem1062.java
@@ -0,0 +1,68 @@
+package com.keunwon.algorithm.bruteforce;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Problem1062 {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int N, K;
+    private static String[] words;
+
+    private static int max = Integer.MIN_VALUE;
+    private static int mask;
+
+    public static void main(String[] args) throws IOException {
+        input();
+        dfs(0, 0);
+    }
+
+    private static void dfs(int x, int depth) {
+        if (K - 5 == depth) {
+            int count = 0;
+
+            for (int i = 0; i < N; i++) {
+                boolean flag = true;
+
+                for (int j = 0; j < words[i].length(); j++) {
+                    if ((mask & (1 << words[i].charAt(j) - 96)) == 0) {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag) { count++; }
+            }
+
+            max = Math.max(max, count);
+            return;
+        }
+
+        for (int i = x; i <= 26; i++) {
+            if ((mask & (1 << i)) == 0) {
+                dfs(i + 1, depth + 1);
+            }
+        }
+    }
+
+    private static void input() throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+        words = new String[N];
+        mask = 0;
+
+        for (int i = 0; i < N; i++) {
+            String word = br.readLine().replaceAll("[antic]", "");
+            words[i] = word;
+        }
+
+        mask |= 1 << (int) 'a' - 96;
+        mask |= 1 << (int) 'n' - 96;
+        mask |= 1 << (int) 't' - 96;
+        mask |= 1 << (int) 'i' - 96;
+        mask |= 1 << (int) 'c' - 96;
+    }
+}
