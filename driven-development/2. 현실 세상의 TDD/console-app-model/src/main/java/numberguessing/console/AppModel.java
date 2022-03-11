142abcd5fa51c2f31b19672d7fe0d4a0c244955c
keunwon <keunwon.c2@gmail.com>
2022-03-12 02:56:04 +0900

    1. 테스트 주도 개발 기초

--- driven-development/2. 현실 세상의 TDD/console-app-model/src/main/java/numberguessing/console/AppModel.java
+++ driven-development/2. 현실 세상의 TDD/console-app-model/src/main/java/numberguessing/console/AppModel.java
@@ -2,23 +2,29 @@ package numberguessing.console;
 
 import numberguessing.PositiveIntegerGenerator;
 
+import java.util.stream.Stream;
+
 public class AppModel {
-    private int answer;
+    private final String SELECT_MODE_MESSAGE = """
+            1: Single player game
+            2: Multiplayer game
+            3: Exit
+            Enter selection:\040""";
+
+    interface Processor {
+        Processor run(String input);
+    }
+
+    private final PositiveIntegerGenerator generator;
+    private final StringBuffer outputBuffer;
     private boolean completed;
-    private boolean singlePlayerMode;
-    private String output;
-    private int tries;
+    private Processor processor;
 
     public AppModel(PositiveIntegerGenerator generator) {
-        answer = generator.generateLessThanOrEqualToHundred();
+        this.generator = generator;
+        outputBuffer = new StringBuffer(SELECT_MODE_MESSAGE);
         completed = false;
-        singlePlayerMode = false;
-        output = """
-                1: Single player game
-                2: Multiplayer game
-                3: Exit
-                Enter selection:\040""";
-        tries = 0;
+        this.processor = this::processModeSelection;
     }
 
     public boolean isCompleted() {
@@ -26,42 +32,91 @@ public class AppModel {
     }
 
     public String flushOutput() {
+        final var output = outputBuffer.toString();
+        outputBuffer.setLength(0);
         return output;
     }
 
     public void processInput(String input) {
-        if (singlePlayerMode) {
-            processSinglePlayerGame(input);
-        } else {
-            processModeSelection(input);
-        }
+        processor = processor.run(input);
     }
 
-    private void processSinglePlayerGame(String input) {
-        tries++;
-        var guess = Integer.parseInt(input);
-        if (guess < answer) {
-            output = """
-                Your guess is too high.
-                Enter your guess:\040""";
-        } else if (guess > answer) {
-            output = """
-                Your guess is too low.
-                Enter your guess:\040""";
-        } else {
-            output = "Correct! %d guesses.%s".formatted(tries, System.lineSeparator());
-        }
-    }
-
-    private void processModeSelection(String input) {
+    private Processor processModeSelection(String input) {
         if ("1".equals(input)) {
-            output = """
-                    Single player game
-                    I'm thinking of a number between 1 and 100.
-                    Enter your guess:\040""";
-            singlePlayerMode = true;
+            println("Single player game");
+            println("I'm thinking of a number between 1 and 100.");
+            print("Enter your guess: ");
+
+            final var answer = generator.generateLessThanOrEqualToHundred();
+            return getSinglePlayerGameProcessor(answer, 1);
+        } else if ("2".equals(input)) {
+            println("Multiplayer game");
+            print("Enter player names separated with commas: ");
+
+            return startMultiPlayerGameProcessor();
         } else {
             completed = true;
         }
+        return null;
+    }
+
+    private Processor startMultiPlayerGameProcessor() {
+        return input -> {
+            println("I'm thinking of a number between 1 and 100.");
+
+            final var players = Stream.of(input.split(","))
+                    .map(String::trim)
+                    .toArray();
+            final var answer = generator.generateLessThanOrEqualToHundred();
+            return getMultiPlayerGameProcessor(players, answer, 1);
+        };
+    }
+
+    private Processor getMultiPlayerGameProcessor(Object[] players, int answer, int tries) {
+        final Object player = players[(tries - 1) % players.length];
+
+        print("Enter %s's guess: ".formatted(player));
+        return input -> {
+            final var guess = Integer.parseInt(input);
+
+            if (guess < answer) {
+                println("%s's guess is too low.".formatted(player));
+            } else if (guess > answer) {
+                println("%s's guess is too high.".formatted(player));
+            } else {
+                println("Correct! %s wins.".formatted(player));
+                print(SELECT_MODE_MESSAGE);
+                return this::processModeSelection;
+            }
+            return getMultiPlayerGameProcessor(players, answer, tries + 1);
+        };
+    }
+
+    private Processor getSinglePlayerGameProcessor(int answer, int tries) {
+        return input -> {
+            final var guess = Integer.parseInt(input);
+            if (guess < answer) {
+                println("Your guess is too low.");
+                print("Enter your guess: ");
+                return getSinglePlayerGameProcessor(answer, tries + 1);
+            } else if (guess > answer) {
+                println("Your guess is too high.");
+                print("Enter your guess: ");
+                return getSinglePlayerGameProcessor(answer, tries + 1);
+            } else {
+                println("Correct! %d %s.".formatted(tries, tries == 1 ? "guess" : "guesses"));
+                print(SELECT_MODE_MESSAGE);
+                return this::processModeSelection;
+            }
+        };
+    }
+
+    private void print(String message) {
+        outputBuffer.append(message);
+    }
+
+    private void println(String line) {
+        outputBuffer.append(line);
+        outputBuffer.append(System.lineSeparator());
     }
 }
--- driven-development/2. 현실 세상의 TDD/console-app/src/main/java/numberguessing/console/App.java
+++ driven-development/2. 현실 세상의 TDD/console-app/src/main/java/numberguessing/console/App.java
@@ -1,11 +1,16 @@
 package numberguessing.console;
 
+import numberguessing.RandomGenerator;
+
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-
+        final var model = new AppModel(new RandomGenerator());
+        final var sc = new Scanner(System.in);
+        runLoop(model, sc);
+        sc.close();
     }
 
     private static void runLoop(AppModel model, Scanner sc) {
