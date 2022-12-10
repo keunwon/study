33162904e39b02b736fabc54b828326d8d86abab
keunwon <keunwon.c2@gmail.com>
2022-12-11 06:17:24 +0900

    doc: 04. 대규모 확장 가능한 콘텐츠 캐싱

---
+++ etc/5_NGINX_COOK_BOOK/shell/mvc_requests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env	bash
+
+count=$1
+echo "반복 횟수: ${count}"
+if [ -z $count ]; then
+	echo "반복 횟수를 입력해주세요"
+	exit 1
+fi
+
+api=http://192.168.45.42
+
+for ((i=1; i<=${count}; i++))
+do
+    curl -X GET "$api/chapter02/" | jq &
+done
+
