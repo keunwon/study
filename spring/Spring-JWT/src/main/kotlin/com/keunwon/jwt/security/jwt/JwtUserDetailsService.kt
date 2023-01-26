e65498edb8090d789d697a5589920cd1e12a9c92
keunwon <keunwon.c2@gmail.com>
2023-01-26 21:32:14 +0900

    REFACTOR: JwtUserDetailsServiceImpl 이동

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/security/jwt/JwtUserDetailsService.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/security/jwt/JwtUserDetailsService.kt
@@ -1,23 +1,18 @@
 package com.keunwon.jwt.security.jwt
 
+import com.keunwon.jwt.domain.User
+import com.keunwon.jwt.domain.UserRepository
 import com.keunwon.jwt.domain.UserRole
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.security.core.Authentication
+import org.springframework.transaction.annotation.Transactional
 
 interface JwtUserDetailsService<T : AbstractCustomUser<out ID>, ID> {
     fun findLoginUser(id: ID): T
     fun findByAuthentication(authentication: Authentication): T?
     fun save(user: T): T
 
-    fun successLogin(id: ID) = findLoginUser(id).apply {
-        failCount = 0
-    }
-
-    fun incFailCount(id: ID) = findLoginUser(id).apply {
-        failCount += 1
-        isActivated = geMaxFailCount(failCount)
-    }
-
-    private fun geMaxFailCount(failCount: Int): Boolean = failCount <= LOGIN_FAIL_COUNT
+    fun overLoginFailCount(failCount: Int): Boolean = failCount <= LOGIN_FAIL_COUNT
 
     companion object {
         const val LOGIN_FAIL_COUNT = 10
@@ -33,3 +28,12 @@ abstract class AbstractCustomUser<ID> {
     abstract var role: UserRole
 }
 
+@Transactional
+class JwtUserDetailsServiceImpl(private val userRepository: UserRepository) : JwtUserDetailsService<User, Long> {
+    override fun findLoginUser(id: Long): User = userRepository.findByIdOrNull(id)!!
+
+    override fun findByAuthentication(authentication: Authentication): User? =
+        userRepository.findByUsername(authentication.principal as String)
+
+    override fun save(user: User): User = userRepository.save(user)
+}
