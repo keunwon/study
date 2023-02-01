f6e5da2f2d701ad0dd86e3d22779afffedf8c5dc
keunwon <keunwon.c2@gmail.com>
2023-02-02 06:03:22 +0900

    REFACTOR: index.adoc 경로 변경

--- spring/Spring-JWT/build.gradle.kts
+++ spring/Spring-JWT/build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.7"
     id("io.spring.dependency-management") version "1.1.0"
-    id ("org.asciidoctor.jvm.convert") version "3.3.2"
+    id("org.asciidoctor.jvm.convert") version "3.3.2"
     kotlin("jvm") version "1.7.22"
     kotlin("plugin.spring") version "1.7.22"
     kotlin("plugin.jpa") version "1.7.22"
@@ -102,7 +102,7 @@ tasks {
         }
         doLast {
             copy {
-                from("${buildDir.path}/docs/asciidoc")
+                from("${buildDir.path}/docs/")
                 into("src/main/resources/static/docs")
             }
         }
