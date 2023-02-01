35549da3b16f9f5ffa6bf6b3c4b06950b6363933
keunwon <keunwon.c2@gmail.com>
2023-02-02 06:07:40 +0900

    REFACTOR: 조회 메서드 변경(기존: findByUserId, 변경:getByUserId)

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/UserRepository.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/UserRepository.kt
@@ -2,6 +2,9 @@ package com.keunwon.jwt.domain
 
 import org.springframework.data.jpa.repository.JpaRepository
 
+fun UserRepository.getByUsername(username: String): User = findByUsername(username)
+    ?: throw IllegalArgumentException("$username 찾을 수 없습니다.")
+
 interface UserRepository : JpaRepository<User, Long> {
     fun findByUsername(username: String): User?
     fun existsByUsername(username: String): Boolean
