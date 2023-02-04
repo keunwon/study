b6bf0e87e0970491a5fa0bbe8a1ef7f6006e44ce
keunwon <keunwon.c2@gmail.com>
2023-02-04 22:52:13 +0900

    REFACTOR: 상속 클래스 변경 (기존: JpaRepository, 변경: CrudRepository)

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/UserRepository.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/UserRepository.kt
@@ -1,12 +1,13 @@
 package com.keunwon.jwt.domain
 
-import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.CrudRepository
 
 fun UserRepository.getByUsername(username: String): User = findByUsername(username)
     ?: throw IllegalArgumentException("$username 찾을 수 없습니다.")
 
-interface UserRepository : JpaRepository<User, Long> {
+interface UserRepository : CrudRepository<User, Long> {
     fun findByUsername(username: String): User?
-    fun existsByUsername(username: String): Boolean
     fun findByEmail(email: String): User?
+    fun existsByUsername(username: String): Boolean
+    fun existsByEmail(email: String): Boolean
 }
