cb4e4d73ab4e5f804aa46dd6e33de8c2f1848d81
keunwon <keunwon.c2@gmail.com>
2023-02-08 18:36:38 +0900

    FEAT: 확장 함수 추가(getById)

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/user/UserRepository.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/domain/user/UserRepository.kt
@@ -1,9 +1,13 @@
 package com.keunwon.jwt.domain.user
 
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.findByIdOrNull
 
 fun UserRepository.getByUsername(username: String): User = findByUsername(username)
     ?: throw IllegalArgumentException("$username 찾을 수 없습니다.")
+
+fun UserRepository.getById(id: Long): User = findByIdOrNull(id)
+    ?: throw IllegalArgumentException("$id 값이 존재하지 않습니다.")
 
 interface UserRepository : CrudRepository<User, Long> {
     fun findByUsername(username: String): User?
