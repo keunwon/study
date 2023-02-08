89bf2fc169232b8b90143d15e34cea3be5ff5acb
keunwon <keunwon.c2@gmail.com>
2023-02-08 22:24:48 +0900

    FEAT: Custom ArgumentResolver 추가

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/config/web/WebConfiguration.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/config/web/WebConfiguration.kt
@@ -1,6 +1,8 @@
 package com.keunwon.jwt.config.web
 
+import com.keunwon.jwt.security.jwt.LoginUserResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
@@ -16,6 +18,12 @@ class WebConfiguration : WebMvcConfigurer {
     }
 
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/resources/**").addResourceLocations("classpath:static/")
+        registry
+            .addResourceHandler("/resources/**").addResourceLocations("classpath:static/")
+    }
+
+    override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
+        resolvers
+            .add(LoginUserResolver())
     }
 }
