dd687037efabc499443509d267487fb6f89febc4
keunwon <keunwon.c2@gmail.com>
2023-02-10 00:17:52 +0900

    FEAT: UnIdentifiedUserException handle 추가
    - 사용자 검증 실패시 발생

--- spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/common/DomainCommonErrorHandler.kt
+++ spring/Spring-JWT/src/main/kotlin/com/keunwon/jwt/common/DomainCommonErrorHandler.kt
@@ -1,6 +1,7 @@
 package com.keunwon.jwt.common
 
 import com.keunwon.jwt.config.LogSupport
+import com.keunwon.jwt.domain.user.UnIdentifiedUserException
 import com.keunwon.jwt.security.jwt.JwtProvider
 import io.jsonwebtoken.ExpiredJwtException
 import io.jsonwebtoken.JwtException
@@ -12,11 +13,20 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class DomainCommonErrorHandler {
     @ExceptionHandler(JwtException::class)
-    fun handleExpiredJwtException(ex: ExpiredJwtException): ResponseEntity<ErrorDto> {
-        val message = JwtProvider.validationErrorMessages[ex::class] ?: "토큰이 유효하지 않습니다"
-        log.warn("> $message")
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-            .body(ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR.value(), message))
+    fun handleExpiredJwtException(exception: ExpiredJwtException): ResponseEntity<ErrorDto> {
+        val message = JwtProvider.validationErrorMessages[exception::class] ?: exception.message
+        log.warn("> 토큰이 유효하지 않습니다.", exception)
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ErrorDto(HttpStatus.INTERNAL_SERVER_ERROR, message))
+    }
+
+    @ExceptionHandler(UnIdentifiedUserException::class)
+    fun handleUnIdentifiedUserException(exception: UnIdentifiedUserException): ResponseEntity<ErrorDto> {
+        log.error("> 사용자 인증을 실패하였습니다.", exception)
+        return ResponseEntity
+            .status(HttpStatus.FORBIDDEN)
+            .body(ErrorDto(HttpStatus.FORBIDDEN, exception.message))
     }
 
     companion object : LogSupport
