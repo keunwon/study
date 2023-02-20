f16e101a95dc7d92cf05e623964ba66496f0d704
keunwon <keunwon.c2@gmail.com>
2023-02-21 01:15:58 +0900

    FEAT: AbstractAggregateRoot 상속한 기본 Entity 추가

--- spring/Spring-JWT/core-jpa/src/main/kotlin/com/github/keunwon/corejpa/BaseEntity.kt
+++ spring/Spring-JWT/core-jpa/src/main/kotlin/com/github/keunwon/corejpa/BaseEntity.kt
@@ -2,6 +2,7 @@ package com.github.keunwon.corejpa
 
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.domain.AbstractAggregateRoot
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
 import javax.persistence.Column
@@ -16,18 +17,33 @@ import javax.persistence.MappedSuperclass
 abstract class BaseEntity(
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
-    val id: Long = 0L,
+    val id: Long,
 
     createDateTime: LocalDateTime = LocalDateTime.now(),
     modifiedDateTime: LocalDateTime = LocalDateTime.now(),
 ) {
     @CreatedDate
-    @Column(nullable = false)
     var createdDateTime: LocalDateTime = createDateTime
         protected set
 
     @LastModifiedDate
+    var modifiedLocalDateTime: LocalDateTime = modifiedDateTime
+        protected set
+}
+
+abstract class BaseRootEntity<A : AbstractAggregateRoot<A>>(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false)
+    val id: Long,
+
+    createDateTime: LocalDateTime = LocalDateTime.now(),
+    modifiedDateTime: LocalDateTime = LocalDateTime.now(),
+) : AbstractAggregateRoot<A>() {
+    @CreatedDate
+    var createdDateTime: LocalDateTime = createDateTime
+        protected set
+
+    @LastModifiedDate
     var modifiedLocalDateTime: LocalDateTime = modifiedDateTime
         protected set
 }
