62287a1f2e10d0d2b9429f6a7b473be847dacf32
keunwon <keunwon.c2@gmail.com>
2025-01-29 00:30:16 +0900

    필요없는 파일 삭제

--- algorithm/kotlin-algorithm/settings.gradle.kts
+++ algorithm/kotlin-algorithm/settings.gradle.kts
@@ -1,5 +1,1 @@
 rootProject.name = "kotlin-algorithm"
-include("src:main:untitled")
-findProject(":src:main:untitled")?.name = "untitled"
-include("src:main:java")
-findProject(":src:main:java")?.name = "java"
