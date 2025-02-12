167224ee4d035a5d5a70a051098beebf1c06a53c
keunwon <keunwon.c2@gmail.com>
2025-02-12 21:46:58 +0900

    백준(삼성 SW 역량 테스트 기출 문제) 풀이

---
+++ algorithm/kotlin-algorithm/src/test/kotlin/com/keunwon/algorithm/baekjoon/Problem20056Test.kt
@@ -0,0 +1,61 @@
+package com.keunwon.algorithm.baekjoon
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class Problem20056Test : StringSpec({
+    "case-1" {
+        val n = 4
+        val k = 1
+        val infos = arrayOf(
+            intArrayOf(1, 1, 5, 2, 2),
+            intArrayOf(1, 4, 7, 1, 6),
+        )
+
+        val actual = Problem20056().solution(n, k, infos)
+
+        actual shouldBe 8
+    }
+
+    "case-2" {
+        val n = 4
+        val k = 2
+        val infos = arrayOf(
+            intArrayOf(1, 1, 5, 2, 2),
+            intArrayOf(1, 4, 7, 1, 6),
+        )
+
+        val actual = Problem20056().solution(n, k, infos)
+
+        actual shouldBe 8
+    }
+
+    "case-3" {
+        val n = 4
+        val k = 3
+        val infos = arrayOf(
+            intArrayOf(1, 1, 5, 2, 2),
+            intArrayOf(1, 4, 7, 1, 6),
+        )
+
+        val actual = Problem20056().solution(n, k, infos)
+
+        actual shouldBe 0
+    }
+
+    "case-4" {
+        val n = 7
+        val k = 3
+        val infos = arrayOf(
+            intArrayOf(1, 3, 5, 2, 4),
+            intArrayOf(2, 3, 5, 2, 6),
+            intArrayOf(5, 2, 9, 1, 7),
+            intArrayOf(6, 2, 1, 3, 5),
+            intArrayOf(4, 4, 2, 4, 2),
+        )
+
+        val actual = Problem20056().solution(n, k, infos)
+
+        actual shouldBe 9
+    }
+})
---
+++ algorithm/kotlin-algorithm/src/test/kotlin/com/keunwon/algorithm/baekjoon/Problem23288Test.kt
@@ -0,0 +1,118 @@
+package com.keunwon.algorithm.baekjoon
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class Problem23288Test : StringSpec({
+    "case-1" {
+        val k = 1
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 4
+    }
+
+    "case-2" {
+        val k = 2
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 8
+    }
+
+    "case-3" {
+        val k = 3
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 14
+    }
+
+    "case-4" {
+        val k = 4
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 18
+    }
+
+    "case-5" {
+        val k = 5
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 24
+    }
+
+    "case-6" {
+        val k = 6
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 28
+    }
+
+    "case-7" {
+        val k = 7
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 43
+    }
+
+    "case-8" {
+        val k = 1000
+        val board = arrayOf(
+            intArrayOf(4, 1, 2, 3, 3),
+            intArrayOf(6, 1, 1, 3, 3),
+            intArrayOf(5, 6, 1, 3, 2),
+            intArrayOf(5, 5, 6, 5, 5),
+        )
+
+        val actual = Problem23288().solution(k, board)
+
+        actual shouldBe 3901
+    }
+})
