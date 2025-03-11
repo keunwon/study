584eaf131d8e50753d173d6c46c63e6247d4d0da
keunwon <keunwon.c2@gmail.com>
2025-03-12 01:34:31 +0900

    프로그래머스 풀이

---
+++ algorithm/kotlin-algorithm/src/test/kotlin/com/keunwon/algorithm/programmers/Lesson388354Test.kt
@@ -0,0 +1,38 @@
+package com.keunwon.algorithm.programmers
+
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+
+class Lesson388354Test : StringSpec({
+    "case-1" {
+        val nodes = intArrayOf(11, 9, 3, 2, 4, 6)
+        val edges = arrayOf(
+            intArrayOf(9, 11),
+            intArrayOf(2, 3),
+            intArrayOf(6, 3),
+            intArrayOf(3, 4),
+        )
+
+        val actual = Lesson388354().solution(nodes, edges)
+
+        actual shouldBe intArrayOf(1, 0)
+    }
+
+    "case-2" {
+        val nodes = intArrayOf(9, 15, 14, 7, 6, 1, 2, 4, 5, 11, 8, 10)
+        val edges = arrayOf(
+            intArrayOf(5, 14),
+            intArrayOf(1, 4),
+            intArrayOf(9, 11),
+            intArrayOf(2, 15),
+            intArrayOf(2, 5),
+            intArrayOf(9, 7),
+            intArrayOf(8, 1),
+            intArrayOf(6, 4),
+        )
+
+        val actual = Lesson388354().solution(nodes, edges)
+
+        actual shouldBe intArrayOf(2, 1)
+    }
+})
